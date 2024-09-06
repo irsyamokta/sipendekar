26005af2a887e14f05ccd5539de7dea2c526fcf6
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-09-07 00:05:58 +0700

    Fix : Responsive feedback

--- resources/views/client/partials/feedback.blade.php
+++ resources/views/client/partials/feedback.blade.php
@@ -10,17 +10,17 @@
     @vite(['resources/css/style.css', 'resources/js/app.js', 'resources/js/index.js', 'resources/js/script.js'])
 </head>
 
-<body class="bg-gradient-to-t from-soft to-white to-16%">
-    <section class="md:h-[100vh] flex flex-col md:flex lg:flex-row">
-        <div class="w-full lg:w-1/2 flex flex-col justify-center items-center px-16 py-16 bg-secondary">
+<body class="bg-white md:bg-gradient-to-t from-soft to-white to-16%">
+    <section class="h-[100vh] flex lg:flex-row">
+        <div class="hidden w-full lg:w-1/2 lg:flex flex-col justify-center items-center px-16 py-16 bg-secondary">
             <a class="flex justify-center" href="https://storyset.com/online" target="_blank">
                 <img class="sm:w-[60%] lg:w-[80%]" src="{{ asset('assets/img/img-feedback.png') }}"
                     alt="Feedback">
             </a>
         </div>
-        <div class="w-full lg:w-1/2 flex flex-col justify-center items-center py-10 gap-10">
+        <div class="w-full lg:w-1/2 flex flex-col justify-center items-center lg:py-10 gap-10">
             @include('client.partials.toast')
-            <div class="flex flex-col justify-center items-center gap-5 bg-white px-20 py-10 rounded-lg shadow-sm">
+            <div class="flex flex-col justify-center items-center gap-5 bg-white md:px-20 md:py-10 rounded-lg shadow-sm">
                 <h1 class="text-2xl font-poppins font-bold text-secondary">Beri Umpan Balik</h1>
                 <form action="{{ route('submitFeedback') }}" method="POST" enctype="multipart/form-data">
                     @csrf
@@ -92,21 +92,18 @@
                             <br>
                             <input type="text" name="name" value="{{ old('name') }}" required
                                 placeholder="Jhon Doe"
-                                class="rounded-lg w-[250px] lg:w-[300px] placeholder:text-xs placeholder:text-[#C4C4C4] md:placeholder:text-sm">
+                                class="rounded-lg w-[250px] lg:w-[300px] placeholder:text-sm placeholder:text-[#C4C4C4]">
                         </div>
                         <div class="mb-5">
                             <label for="feedback" class="text-sm">Ulasan atau masukkan</label>
                             <br>
                             <textarea type="text" rows="4" name="feedback" required placeholder="Deskripsikan pengalaman Anda"
-                                class="rounded-lg w-[250px] lg:w-[300px] placeholder:text-xs placeholder:text-[#C4C4C4] md:placeholder:text-sm">{{ old('feedback') }}</textarea>
+                                class="rounded-lg w-[250px] lg:w-[300px] placeholder:text-sm placeholder:text-[#C4C4C4]">{{ old('feedback') }}</textarea>
                         </div>
                         <button
                             class="w-40 h-10 mt-5 rounded-[30px] bg-secondary text-primary hover:bg-active hover:text-primary duration-300 ease-linear">Submit</button>
                     </div>
                 </form>
-                <p class="mt-5 text-xs text-secondary font-regular">Copyright © SiPendekar {{ date('Y') }}. All
-                    Right
-                    Reserves</p>
             </div>
         </div>
     </section>
