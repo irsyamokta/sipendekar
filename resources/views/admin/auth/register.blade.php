34b5060786f3c3f506c5fbeba67c26b7006adddb
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-09-07 15:37:41 +0700

    Fix : UI register

--- resources/views/admin/auth/register.blade.php
+++ resources/views/admin/auth/register.blade.php
@@ -11,14 +11,14 @@
 </head>
 
 <body class="bg-gradient-to-t from-soft to-white to-16%">
-    <section class="md:h-[100vh] flex flex-col md:flex lg:flex-row">
-        <div class="w-full lg:w-1/2 flex flex-col justify-center items-center px-16 py-16 bg-secondary">
+    <section class="h-[100vh] flex lg:flex-row">
+        <div class="hidden w-full lg:w-1/2 lg:flex flex-col justify-center items-center px-16 py-16 bg-secondary">
             <img class="sm:w-[60%] lg:w-[80%]" src="{{ asset('assets/img/img-login.png') }}" alt="img-login">
             <p class="text-primary text-center text-sm md:text-lg">Silakan masuk dengan akun Anda. Jika Anda lupa
                 password silakan hubungi staff bagian IT</p>
         </div>
         <div class="w-full lg:w-1/2 flex flex-col justify-center items-center py-10 gap-10">
-            <img src="{{ asset('assets/logo/logo-horizontal-color.png') }}" alt="logo sipendekar" class="w-80">
+            <img src="{{ asset('assets/logo/logo-horizontal-color.png') }}" alt="logo sipendekar" class="w-60 md:w-80">
             <form action="{{ route('register') }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="flex flex-col justify-center items-center">
@@ -26,7 +26,7 @@
                         <label for="nama_lengkap" class="text-sm">Nama Lengkap</label>
                         <br>
                         <input type="text" name="nama_lengkap" value="{{ old('nama_lengkap') }}" required autofocus
-                            autocomplete="nama_lengkap" placeholder="John Doe" class="rounded-[30px] w-[250px] lg:w-[350px] placeholder:text-xs placeholder:text-[#C4C4C4]">
+                            autocomplete="nama_lengkap" placeholder="John Doe" class="rounded-lg w-[250px] lg:w-[350px] placeholder:text-sm placeholder:text-[#C4C4C4]">
                         @if ($errors->any())
                             <p class="mt-3 text-red-500 text-xs">{{ $errors->first('nama_lengkap') }}</p>
                         @endif
@@ -35,13 +35,13 @@
                         <label for="email" class="text-sm">Email</label>
                         <br>
                         <input type="email" name="email" value="{{ old('email') }}" required autofocus
-                            autocomplete="email" placeholder="example@gmail.com" class="rounded-[30px] w-[250px] lg:w-[350px] placeholder:text-xs placeholder:text-[#C4C4C4]">
+                            autocomplete="email" placeholder="example@gmail.com" class="rounded-lg w-[250px] lg:w-[350px] placeholder:text-sm placeholder:text-[#C4C4C4]">
                     </div>
                     <div class="mb-5">
                         <label for="password" class="text-sm">Password</label>
                         <br>
                         <input type="password" name="password" required autocomplete="current-password" placeholder="Password"
-                            class="rounded-[30px] w-[250px] lg:w-[350px] placeholder:text-xs placeholder:text-[#C4C4C4]">
+                            class="rounded-lg w-[250px] lg:w-[350px] placeholder:text-sm placeholder:text-[#C4C4C4]">
                         @if ($errors->has('password'))
                             <p class="mt-3 text-red-500 text-xs">{{ $errors->first('password') }}</p>
                         @endif
@@ -50,7 +50,7 @@
                         <label for="password_confirmation" class="text-sm">Konfirmasi Password</label>
                         <br>
                         <input type="password" name="password_confirmation" required autocomplete="current-password" placeholder="Konfirmasi Password"
-                            class="rounded-[30px] w-[250px] lg:w-[350px] placeholder:text-xs placeholder:text-[#C4C4C4]">
+                            class="rounded-lg w-[250px] lg:w-[350px] placeholder:text-sm placeholder:text-[#C4C4C4]">
                         @if ($errors->has('password_confirmation'))
                             <p class="mt-3 text-red-500 text-xs">{{ $errors->first('password_confirmation') }}</p>
                         @endif
