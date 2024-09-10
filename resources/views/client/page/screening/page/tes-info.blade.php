cd83b7a2ac7d1622fbd4321406c2a66e88a4ce22
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-09-10 17:29:23 +0700

    Feature : Feedback after test

--- resources/views/client/page/screening/page/tes-info.blade.php
+++ resources/views/client/page/screening/page/tes-info.blade.php
@@ -8,7 +8,8 @@
                 class="mt-5 text-3xl md:text-4xl lg:text-3xl xl:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-accent to-secondary">@lang('screening.result.title')</h1>
             <img src="{{ asset('assets/img/img-finish.png') }}" alt="result"
                 class="w-56 md:w-96 lg:w-56 xl:w-80 mt-5">
-            <p class="text-xs md:text-base lg:text-sm xl:text-base px-4 lg:px-40 xl:px-56 mt-5">@lang('screening.result.description')</p>
+            <p class="text-xs md:text-base lg:text-sm xl:text-base px-4 lg:px-40 xl:px-56 mt-5 mb-10">@lang('screening.result.description')</p>
+            @include('client.partials.toggle-modal')
         </div>
     </section>
 @endsection
