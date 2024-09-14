0e6177a1626e8e31dcee633759e2100925632474
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-09-14 23:01:50 +0700

    Fix : Laporan feedback

--- resources/views/admin/dashboard/partials/tables/tables-feedback.blade.php
+++ resources/views/admin/dashboard/partials/tables/tables-feedback.blade.php
@@ -1,13 +1,13 @@
 <table class="w-full text-sm text-center rtl:text-right text-gray-500 dark:text-gray-400">
     <thead class="text-xs text-white uppercase bg-secondary dark:bg-meta-4 dark:text-gray-400">
         <tr>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" class="px-5 py-3">
                 No
             </th>
             <th scope="col" class="px-5 py-3 whitespace-nowrap">
                 Nama Lengkap
             </th>
-            <th scope="col" class="px-30 py-3">
+            <th scope="col" class="px-5 py-3">
                 Asal Sekolah
             </th>
             <th scope="col" class="px-5 py-3">
@@ -21,24 +21,24 @@
     <tbody>
         @if (count($feedback) <= 0)
             <tr>
-                <td colspan="4" class="text-center py-8">Tidak ada data</td>
+                <td colspan="2" class="text-start py-8">Tidak ada data</td>
             </tr>
         @else
             @foreach ($feedback as $row)
                 <tr class="bg-white border-b dark:bg-boxdark">
-                    <th scope="row" class="px-6 py-4 font-regular whitespace-nowrap dark:text-white">
+                    <th scope="row" class="px-3 py-4 font-regular whitespace-nowrap dark:text-white">
                         {{ $loop->iteration }}
                     </th>
-                    <th scope="row" class="px-6 py-4 font-regular whitespace-nowrap dark:text-white">
+                    <th scope="row" class="px-3 py-4 font-regular whitespace-nowrap dark:text-white">
                         {{ $row->nama }}
                     </th>
                     <th scope="row" class="px-3 py-4 font-regular dark:text-white">
                         {{ $row->sekolah }}
                     </th>
-                    <th scope="row" class="px-6 py-4 font-regular whitespace-nowrap dark:text-white">
+                    <th scope="row" class="px-3 py-4 font-regular whitespace-nowrap overflow-y-auto max-w-[200px] no-scroll dark:text-white">
                         {{ $row->umpan_balik }}
                     </th>
-                    <th scope="row" class="px-3 md:px-20 py-4 font-regular dark:text-white whitespace-nowrap">
+                    <th scope="row" class="px-3 py-4 font-regular dark:text-white whitespace-nowrap">
                         {{ $row->created_at->locale('id')->isoFormat('dddd, D MMMM YYYY') }}
                     </th>
                 </tr>
